4ab617b6220b065d3b8f1be8dc7b63af002324de
Amber <asorenson@klipsu.com>
2023-03-10 13:31:00 -0600

    updated code to make it cleaner

--- app/src/Components/Player/ProfilePage.tsx
+++ app/src/Components/Player/ProfilePage.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { create } from 'zustand';
+import { last } from '@tanstack/react-router';
 
 interface PlayerStore {
   firstName: string;
   setFirstName: (name: string) => void;
+  lastName: string;
+  setLastName: (name: string) => void;
+  foreground: string;
+  setForeground: (foreground: string) => void;
+  background: string;
+  setBackground: (background: string) => void;
 }
 
 const usePlayerStore = create<PlayerStore>(set => ({
@@ -12,25 +19,67 @@ const usePlayerStore = create<PlayerStore>(set => ({
     set({
       firstName: name,
     }),
+  lastName: '',
+  setLastName: name =>
+    set({
+      lastName: name,
+    }),
+  foreground: '',
+  setForeground: foreground =>
+    set({
+      foreground: foreground,
+    }),
+  background: '',
+  setBackground: background =>
+    set({
+      background: background,
+    }),
 }));
 function PlayerForm() {
   const { firstName, setFirstName } = usePlayerStore();
-
-  const handleInputNameChange = (event: React.ChangeEvent<HTMLFormElement>) => {
-    setFirstName(event.target.value);
-  };
+  const { lastName, setLastName } = usePlayerStore();
+  const { foreground, setForeground } = usePlayerStore();
+  const { background, setBackground } = usePlayerStore();
 
   // @ts-ignore
   const handleNameSubmit = (event: React.FormEven<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(firstName);
+    console.log(firstName, lastName, foreground, background);
   };
 
   return (
     <form onSubmit={handleNameSubmit}>
       <label>
         First Name:
-        <input type="text" value={firstName} onChange={handleInputNameChange} />
+        <input
+          type="text"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+      </label>
+      <label>
+        Last Name:
+        <input
+          type="text"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
+      </label>
+      <label>
+        Foreground:
+        <input
+          type="text"
+          value={foreground}
+          onChange={e => setForeground(e.target.value)}
+        />
+      </label>
+      <label>
+        Background:
+        <input
+          type="text"
+          value={background}
+          onChange={e => setBackground(e.target.value)}
+        />
       </label>
       <button type="submit">Submit</button>
     </form>
