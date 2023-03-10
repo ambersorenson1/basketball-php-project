81f1dd7c16cc39b46e2e5e0086c1a7685f14cc1e
Amber <asorenson@klipsu.com>
2023-03-10 09:18:26 -0600

    completed code so that all teams are posted to the dom

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import TeamForm from './Components/Team/TeamForm';
 
 const queryClient = new QueryClient();
 
@@ -7,7 +8,9 @@ function App() {
   // @ts-ignore
   return (
     <div>
-      <QueryClientProvider client={queryClient}></QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <TeamForm />
+      </QueryClientProvider>
     </div>
   );
 }
--- app/src/Components/Team/TeamForm.tsx
+++ app/src/Components/Team/TeamForm.tsx
@@ -1,40 +1,48 @@
-import React { useState } from "react";
+import React from 'react';
 import { Team } from './TeamInterface';
-import {create} from "zustand";
+import { create } from 'zustand';
 import axios from 'axios';
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from '@tanstack/react-query';
 
 interface TeamStore {
-    teams: Team[];
-    setTeams: (teams: Team[]) => void;
+  teams: Team[];
+  setTeams: (teams: Team[]) => void;
 }
 
 const useTeamStore = create<TeamStore>(set => ({
-    teams: [],
-    setTeams: teams => set ({ teams })
+  teams: [],
+  setTeams: teams => set({ teams }),
 }));
-
 function TeamForm() {
-const { teams, setTeams } = useTeamStore();
-
-const fetchTeams = async (): Promise<Team[]> => {
-    try{
-        const response = await axios.get('https://localhost:8000/api/teams');
-        return response.data;
+  const { teams, setTeams } = useTeamStore();
+  const fetchTeams = async (): Promise<Team[]> => {
+    try {
+      const response = await axios.get('http://localhost:8000/api/teams');
+      return response.data;
     } catch (error) {
-        console.error(error);
-        return [];
+      console.error(error);
+      return [];
     }
-};
-    useQuery(['team'], fetchTeams, {
-        onSuccess: data => {
-            setTeams(data);
-        },
-    });
+  };
 
-    if(!teams)
+  useQuery(['team'], fetchTeams, {
+    onSuccess: data => {
+      setTeams(data);
+    },
+  });
+  if (!teams.length) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h1>Teams:</h1>
+      <ul>
+        {teams.map((team, index) => (
+          <li key={`${index}-${team.team_id}`}>{team.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TeamForm;
-
-
