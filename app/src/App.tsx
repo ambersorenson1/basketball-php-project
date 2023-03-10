e773dcee1bcbfaa3a19117a698ba71cfbf718a95
Amber <asorenson@klipsu.com>
2023-03-10 08:33:56 -0600

    clean

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,3 +1,15 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+function App() {
+  // @ts-ignore
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}></QueryClientProvider>
+    </div>
+  );
 }
+
+export default App;
