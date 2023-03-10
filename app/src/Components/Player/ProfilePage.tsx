ad42a34915f101fb14d1b506ffec9391db3e0b67
Amber <asorenson@klipsu.com>
2023-03-10 12:33:07 -0600

    updated the player file to allow a user to enter their first name with a submit button  and console log the name

--- app/src/Components/Player/ProfilePage.tsx
+++ app/src/Components/Player/ProfilePage.tsx
@@ -13,5 +13,27 @@ const usePlayerStore = create<PlayerStore>(set => ({
       firstName: name,
     }),
 }));
-function PlayerForm() {}
+function PlayerForm() {
+  const { firstName, setFirstName } = usePlayerStore();
+
+  const handleInputNameChange = (event: React.ChangeEvent<HTMLFormElement>) => {
+    setFirstName(event.target.value);
+  };
+
+  // @ts-ignore
+  const handleNameSubmit = (event: React.FormEven<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(firstName);
+  };
+
+  return (
+    <form onSubmit={handleNameSubmit}>
+      <label>
+        First Name:
+        <input type="text" value={firstName} onChange={handleInputNameChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 export default PlayerForm;
