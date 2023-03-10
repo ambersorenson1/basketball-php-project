41811d97e9250294374cee2d9d4a8d99447e3db7
Amber <asorenson@klipsu.com>
2023-03-10 11:12:18 -0600

    added basic layout of Navbar without styling

---
+++ app/src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function BasketballNavBar() {
+  return (
+    <nav>
+      <div>
+        <Link to="/home">My Basketball App</Link>
+      </div>
+    </nav>
+  );
+}
+export default BasketballNavBar;
