5878705c0b9126c7c33fa8b3f89d516ccf43fc24
Amber <asorenson@klipsu.com>
2023-03-31 08:28:06 -0500

    updated the tournament DTO

--- server/src/DTO/TournamentDTO.php
+++ server/src/DTO/TournamentDTO.php
@@ -6,31 +6,55 @@ use DateTime;
 
 class TournamentDTO {
     public string $tournamentName;
-    private DateTime $startDate;
-    private DateTime $endDate;
+    public DateTime $startDate;
+    public DateTime $endDate;
 
-    public function getName(): string {
+    /**
+     * @return string
+     */
+    public function getTournamentName(): string
+    {
         return $this->tournamentName;
     }
 
-    public function setName(string $tournamentName): void {
+    /**
+     * @param string $tournamentName
+     */
+    public function setTournamentName(string $tournamentName): void
+    {
         $this->tournamentName = $tournamentName;
     }
 
+    /**
+     * @return DateTime
+     */
     public function getStartDate(): DateTime
     {
         return $this->startDate;
     }
 
-    public function setStartDate(DateTime $startDate): void {
+    /**
+     * @param DateTime $startDate
+     */
+    public function setStartDate(DateTime $startDate): void
+    {
         $this->startDate = $startDate;
     }
 
-    public function getEndDate(): DateTime {
+    /**
+     * @return DateTime
+     */
+    public function getEndDate(): DateTime
+    {
         return $this->endDate;
     }
 
-    public function setEndDate(DateTime $endDate): void {
+    /**
+     * @param DateTime $endDate
+     */
+    public function setEndDate(DateTime $endDate): void
+    {
         $this->endDate = $endDate;
     }
+
 }
